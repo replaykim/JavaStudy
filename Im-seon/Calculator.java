c4d64b722e7e3548101ec61fd2f2797d642ab3ce
KangImseon <dlatjs4380@gmail.com>
2015-10-15 23:30:55 +0900

    complete

---
+++ Im-seon/Calculator.java
@@ -0,0 +1,30 @@
+package test_564602316;
+
+import java.util.Scanner;
+
+public class Calculator {
+	public static void main(String [] args){
+		
+		int c;
+		int a = 0;
+		int b = 0;
+		System.out.println("ÀÔ·Â ¤¡");
+		Scanner scanner = new Scanner(System.in);
+		c= scanner.nextInt();
+		a = scanner.nextInt();
+		b = scanner.nextInt();
+		
+	
+		
+		System.out.println(a+"+"+b+"="+(a+b));
+		System.out.println(a+"-"+b+"="+(a-b));
+		System.out.println(a+"*"+b+"="+a*b);
+		System.out.println(a+"/"+b+"="+a/b);
+		
+		
+		
+		
+		
+	}
+
+}
