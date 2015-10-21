b7c9531b499f9c7f0b3aca00f13562722da89f89
HyunArm Song <rumination.ha@gmail.com>
2015-10-21 14:12:16 +0900

    승찬 피드백

--- Seung-chan/Chapter 1/Chapter1.java
+++ Seung-chan/Chapter 1/Chapter1.java
@@ -2,12 +2,17 @@ package chappi12_1014;
 
 import java.util.Scanner;
 
+/*
+* Scanner를 두개로 선언해서 따로 사용했는데 불필요한 선언임. Scanner하나로 가용가능함
+* 변수선언은 항상 맨처음에 해주고 사용하는걸 추천함
+* 사용될때 그때 그때 선언하면 나중에 헷갈림.
+*/
+
 public class Chapter1 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.print("정수를 입력해주세요.:");
         int Input = sc.nextInt();
-
         Scanner sc2 = new Scanner(System.in);
         System.out.print("또 다른 정수를 입력해주세요.:");
         int Input2 = sc2.nextInt();
