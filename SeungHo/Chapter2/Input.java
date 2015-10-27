945aa143666b52553dfc36613ed940d80bbafdb9
seunghohyun <scsc3313@naver.com>
2015-10-27 11:20:07 +0900

    Update Input.java

--- SeungHo/Chapter2/Input.java
+++ SeungHo/Chapter2/Input.java
@@ -13,10 +13,12 @@ public class Input {
     //Scanner같이 한번선언하고 여러곳에서 사용할경우에는
     //(이번 코드는 inputData메소드에서만 사용하지만 프로그램이 커진다면..)
     //생성자에서 선언하고 사용하는게 효율적!
+    
+    public Input(){
+        scan = new Scanner(System.in);
+    }
 
     public void inputData(){
-
-        scan = new Scanner(System.in);
         firstNum = scan.nextInt();
         secondNum = scan.nextInt();
     }
